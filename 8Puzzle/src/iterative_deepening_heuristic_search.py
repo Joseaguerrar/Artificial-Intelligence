114c147a7a289419979a5ecaf9f3b035b2a0463d
josermq0 <173841844+josermq0@users.noreply.github.com>
2025-09-06 13:50:20 -0600

    Declare, document the IterativeDeepeningHeuristicSearch class

---
+++ 8Puzzle/src/iterative_deepening_heuristic_search.py
@@ -0,0 +1,34 @@
+"""
+iterative_deepening_heuristic_search.py
+
+This module is a concrete subclass that inherits from the SearchAlgorithm class.
+
+Classes:
+  IterativeDeepeningHeuristicSearch: Represents the iterative deepening heuristic search algorithm,
+  which will be used to solve an 8 puzzle.
+"""
+
+from search_algorithm import SearchAlgorithm
+
+class IterativeDeepeningHeuristicSearch(SearchAlgorithm):
+  """
+  Class that represents the iterative deepening heuristic search algorithm.
+  """
+  def __init__(self):
+    """
+    Initialize a IterativeDeepeningHeuristicSearch instance
+    """
+    # Call the base class constructor
+    pass
+
+  def find(self, initial_state):
+    """
+    Use the iterative deepening heuristic search algorithm to find the desired state.
+
+    Args:
+      (EightPuzzle): An 8 puzzle with a randomized initial state.
+
+    Note:
+      Implementation of the base class abstract method.
+    """
+    pass
