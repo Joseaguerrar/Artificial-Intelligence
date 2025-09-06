73c9c6b2420910a3fe17976e5a38217dead37e29
josermq0 <173841844+josermq0@users.noreply.github.com>
2025-09-06 08:26:48 -0600

    Add AIMA library initial files

---
+++ 8Puzzle/src/main.py
@@ -0,0 +1,10 @@
+def main():
+  """
+  Main function for program that solves an 8 puzzle using the following
+  algorithms: Breadth First Search (BFs), Greedy Breadth First Heuristic Search
+  (GBFHS), Iterative Deepening Search (IDS), Iterative Deepening Heuristic
+  Search (IDHS).
+  """
+
+if __name__ == "__main__":
+    main()
