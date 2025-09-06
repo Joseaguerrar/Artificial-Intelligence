a98da4f67d847ac2acbcf999ff39d19deefd5814
josermq0 <173841844+josermq0@users.noreply.github.com>
2025-09-06 13:35:38 -0600

    Add documentation, and removed implementations

--- 8Puzzle/src/search_algorithm.py
+++ 8Puzzle/src/search_algorithm.py
@@ -1,17 +1,46 @@
+"""
+search_algorithm.py
+
+This module provides an abstract-base class for search algorithms.
+
+Classes:
+    SearchAlgorithm: Represents a search algorithm, which will be used to solve an 8 puzzle.
+
+"""
+
 from abc import ABC, abstractmethod
-import time
 
 class SearchAlgorithm(ABC):
+  """
+  Class that represents a search algorithm.
+
+  Attributes:
+    elapsed_times (list): Stores the elapsed time for each iteration.
+  """
   def __init__(self):
-    self.elapsed_times = []
+    """
+    Initialize a SearchAlgorithm instance
+    """
+    # Initialize elapsed_times as an empty list
+    pass
 
   def run(self, initial_state):
-    start_time = time.time()
-    self.solve(initial_state)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    self.elapsed_times.append(elapsed_time)
+    """
+    Run the search algorithm, and measure its execution time.
+
+    Args:
+      initial_state (EightPuzzle): An 8 puzzle with a randomized initial state.
+    """
+    # Record the start and end time when finding the goal state for an 8 puzzle. Afterwards, print
+    # the results to a .txt file.
+    pass
 
   @abstractmethod
   def solve(self, initial_state):
+    """
+    Use a search algorithm to find the desired state.
+
+    Note:
+      This method must be implemented by concrete subclasses.
+    """
     pass
