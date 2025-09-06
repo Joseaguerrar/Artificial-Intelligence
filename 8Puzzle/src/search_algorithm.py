ccf1a162d45bc9afabb1133a371fcd34e0142624
josermq0 <173841844+josermq0@users.noreply.github.com>
2025-09-06 13:15:53 -0600

    Implement constructor

--- 8Puzzle/src/search_algorithm.py
+++ 8Puzzle/src/search_algorithm.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
+import time
 
 class SearchAlgorithm(ABC):
-  def __init__(self, iterations_count):
+  def __init__(self):
+    self.elapsed_times = []
 
-  def run(self):
-
+  def run(self, initial_state):
+  
   @abstractmethod
   def solve(self, initial_state):
     pass
