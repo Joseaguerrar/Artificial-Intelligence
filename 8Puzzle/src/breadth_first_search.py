c6988105ce5e4d176cb7bdb764a288d3a3e4d499
josermq0 <173841844+josermq0@users.noreply.github.com>
2025-09-06 13:44:19 -0600

    Declare, document the BreadthFirstSearch class

---
+++ 8Puzzle/src/breadth_first_search.py
@@ -0,0 +1,34 @@
+"""
+breadth_first_search.py
+
+This module is a concrete subclass that inherits from the SearchAlgorithm class.
+
+Classes:
+  BreadthFirstSearch: Represents the breadth first search algorithm, which will be used to solve
+  an 8 puzzle.
+"""
+
+from search_algorithm import SearchAlgorithm
+
+class BreadthFirstSearch(SearchAlgorithm):
+  """
+  Class that represents the breadth first search algorithm.
+  """
+  def __init__(self):
+    """
+    Initialize a BreadthFirstSearch instance
+    """
+    # Call the base class constructor
+    pass
+
+  def find(self, initial_state):
+    """
+    Use the breadth first search algorithm to find the desired state.
+
+    Args:
+      (EightPuzzle): An 8 puzzle with a randomized initial state.
+
+    Note:
+      Implementation of the base class abstract method.
+    """
+    pass
