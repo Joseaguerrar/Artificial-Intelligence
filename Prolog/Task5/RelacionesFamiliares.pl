f083d95d6862e6da7cf1003f30cbfc65b63c5562
Jerson <jerson.bonilla.fer@gmail.com>
2025-10-03 19:02:06 -0600

    FIxing order and typos.

--- Prolog/Task5/RelacionesFamiliares.pl
+++ Prolog/Task5/RelacionesFamiliares.pl
@@ -92,12 +92,15 @@ pom(leonarda, virgilio).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Rules
 
+%%%%%%% Papá %%%%%%%
 % X es papá de Y (es pom y es hombre)
 papa(X, Y) :- pom(X, Y), hombre(X).
 
+%%%%%%% Mamá %%%%%%%
 % X es mamá de Y (es pom y es mujer)
 mama(X, Y) :- pom(X, Y), mujer(X).
 
+%%%%%%% Hermanos %%%%%%%
 % X es hermano o hermana de Y (comparten padres o madres)
 hermanx(X, Y) :- pom(Z, X), pom(Z, Y) , X \= Y.
 
@@ -107,15 +110,23 @@ hermano(X, Y) :- hermanx(X, Y), hombre(X).
 % X es hermana de Y (son hermanxs y es mujer)
 hermana(X, Y) :- hermanx(X, Y), mujer(X).
 
+%%%%%%% Hermanos totales %%%%%%%
 % X es hermano o hermana total de Y (comparten papá y mamá)
 hermanxtotal(X, Y) :- papa(Z, X), papa(Z, Y) , mama(A, X), mama(A, Y).
 
+%%%%%%% Medio hermanos %%%%%%%
 % X es medio hermano o media hermana de Y (son hermanos pero no totales)
 mediohermanx(X, Y) :- hermanx(X, Y), \+ hermanxtotal(X,Y).
 
+%%%%%%% Abuelos %%%%%%%
 % X es abuelo de Y (X es pom de Z y Z es pom de Y)
 abuelx(X, Y) :- pom(X, Z), pom(Z, Y).
 
+% X es abuelo de Y si es abuelo o abuela y es hombre.
+abuelo(X, Y) :- abuelx(X, Y), hombre(X).
+
+% X es abuela de Y si es abuelo o abuela y es mujer.
+abuela(X, Y) :- abuelx(X, Y), mujer(X).
 
 %%%%%%% Reglas auxilares %%%%%%%
 
@@ -128,24 +139,17 @@ hijx(H, P) :- pom(P, H).
 % tiene_hijxs: cierta persona tiene al menos un/a hijx (se ocupa para cuñadx)
 tiene_hijxs(X) :- pom(X, _).
 
-%%%%%%% Abuelos %%%%%%%
-% X es abuelo de Y si es abuelo o abuela y es hombre.
-abuelo(X, Y) :- abuelx(X, Y), hombre(X).
-
-% X es abuela de Y si es abuelo o abuela y es mujer.
-abuela(X, Y) :- abuelx(X, Y), mujer(X).
-
 %%%%%%% Cuñadx (que tenga hijxs)%%%%%%%
 % 1. Hermanx de la pareja
 % 2. Pareja de hermanox
 
 % X es cuñadx de Y si Y es pareja de Z y X es hermano de Z
 % o si X es pareja de Z y Z es hermanx de Y.
-cunnadx(X, Y) :- (pareja(Y, Z), hermanx(X, Z));
+cunhadx(X, Y) :- (pareja(Y, Z), hermanx(X, Z));
     (hermanx(Z, Y), pareja(X, Z)).
 
 % X es cuñado de Y si es cuñado o cuñada y es hombre.
-cunnado(X, Y) :- cunnadx(X, Y), hombre(X).
+cunhado(X, Y) :- cunhadx(X, Y), hombre(X).
 
 % X es cuñado de Y si es cuñado o cuñada y es mujer.
-cunnada(X, Y) :- cunnadx(X, Y), mujer(X).
+cunhado(X, Y) :- cunhadx(X, Y), mujer(X).
